7a5d420eb367f1f6b4cbe8c7eeccfb153179ac78
sangeethkc <keyceesangi@gmail.com>
2022-02-16 20:39:25 +0530

    styling added

--- Header.php
+++ Header.php
@@ -4,7 +4,10 @@
 
 <!DOCTYPE html>
 <header>
-    
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto+Serif:wght@200&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="style.css">
 </header>
 <nav>
     <div class="wrapper">
@@ -21,5 +24,6 @@
                 }
             ?>  
         </ul>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
     </div>
 </nav>
